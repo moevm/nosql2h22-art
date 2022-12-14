3f397b40e5c103cd1d0ec235398a9eac5c9d8817
Ирина Андрух <andruh808@gmail.com>
2022-12-14 03:22:17 +0300

    edit_art

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,6 +7,7 @@ CORS(app, support_credentials=True)
 
 app.add_url_rule('/', methods=['POST'], view_func=routes.clear_tmp)
 app.add_url_rule('/add_art', methods=['POST'], view_func=routes.add_art)
+app.add_url_rule('/art/<id>', methods=['PUT'], view_func=routes.update_art)
 app.add_url_rule('/import_arts', methods=['POST'], view_func=routes.reimport_arts)
 app.add_url_rule('/get_arts', methods=['GET'], view_func=routes.get_arts)
 app.add_url_rule('/get_arts_by_filter', methods=['POST'], view_func=routes.get_arts_by_filter)
@@ -17,4 +18,4 @@ app.add_url_rule('/get_types', methods=['GET'], view_func=routes.get_types)
 app.add_url_rule('/recreate_table', methods=['GET'], view_func=routes.recreate_table)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port="8080")
