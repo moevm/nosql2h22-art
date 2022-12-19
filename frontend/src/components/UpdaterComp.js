b964b16e971180178f451911423303bf4c97fe20
Ирина Андрух <andruh808@gmail.com>
2022-12-19 20:57:25 +0300

    default_values_fix

--- frontend/src/components/UpdaterComp.js
+++ frontend/src/components/UpdaterComp.js
@@ -1,10 +1,11 @@
-import {Typography, TextField, Grid, Button} from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import DescriptionEditor from "./DescriptionEditor";
 import '../App/App.css';
+import Axios from "axios";
 
 
-function UpdaterComp({dataToPass, hide}) {
+function UpdaterComp({ dataToPass, hide }) {
     const [desc_editor, setDescEditor] = React.useState(false);
     const [data, setData] = useState(dataToPass);
 
@@ -18,7 +19,26 @@ function UpdaterComp({dataToPass, hide}) {
         setDescEditor(false);
     }
 
-    function submit() {}
+    function submit() {
+        const url = "http://localhost:5000/art/" + data.artworkid;
+        console.log("url put:", url);
+
+        Axios.put(url, {
+            name: data.name,
+            author: data.author,
+            description: data.description,
+            start_year: data.start_year,
+            end_year: data.end_year,
+            materials: data.materials,
+            type: data.type,
+            museum_name: data.museum_name,
+            genre: data.genre,
+            url: data.url
+        }).then(r => {
+            window.location.reload();
+            console.log(r.data)
+        });
+    }
 
     return (
         <div className="editorMenu">
@@ -33,35 +53,53 @@ function UpdaterComp({dataToPass, hide}) {
                 {/*        /></Button>*/}
                 {/*</Grid>*/}
                 <Grid item xs={12}>
-                    <TextField value={data.name} onChange={(e) => { data.name = e.target.value; }} size='small' fullWidth={true} label="Name" variant="outlined" required />
+                    <TextField defaultValue={data.name} onChange={(e) => {
+                        data.name = e.target.value;
+                    }} size='small' fullWidth={true} label="Name" variant="outlined" required />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField value={data.author} onChange={(e) => { data.author = e.target.value }} size='small' fullWidth={true} label="Author" variant="outlined" required />
+                    <TextField defaultValue={data.author} onChange={(e) => {
+                        data.author = e.target.value
+                    }} size='small' fullWidth={true} label="Author" variant="outlined" required />
                 </Grid>
                 <Grid item xs={12}>
                     <Button variant='outlined' color='primary' onClick={handleDescOpen}>Description</Button>
                     <DescriptionEditor dataToPass={data.description} is_open={desc_editor} func={handleDescClose} />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField value={data.museum_name} onChange={(e) => {data.museum_name = e.target.value;}} size='small' fullWidth={true} label="Museum name & address" variant="outlined" required/>
+                    <TextField defaultValue={data.museum_name} onChange={(e) => {
+                        data.museum_name = e.target.value;
+                    }} size='small' fullWidth={true} label="Museum name & address" variant="outlined" required />
                 </Grid>
                 <Grid item xs={6}>
-                    <TextField value={data.start_year} onChange={(e) => {data.start_year = +(e.target.value)}} type="number" label="Start year" size='small' fullWidth={true}  variant="outlined" required/>
+                    <TextField defaultValue={data.start_year} onChange={(e) => {
+                        data.start_year = +(e.target.value)
+                    }} type="number" label="Start year" size='small' fullWidth={true} variant="outlined" required />
                 </Grid>
                 <Grid item xs={6}>
-                    <TextField value={data.end_year} onChange={(e) => {data.end_year = +(e.target.value)}} type="number" label="End year"  size='small' fullWidth={true}  variant="outlined" required/>
+                    <TextField defaultValue={data.end_year} onChange={(e) => {
+                        data.end_year = +(e.target.value)
+                    }} type="number" label="End year" size='small' fullWidth={true} variant="outlined" required />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField value={data.genre} onChange={(e) => { data.genre = e.target.value }} size='small' fullWidth={true} label="Genre" variant="outlined" required />
+                    <TextField defaultValue={data.genre} onChange={(e) => {
+                        data.genre = e.target.value
+                    }} size='small' fullWidth={true} label="Genre" variant="outlined" required />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField value={data.materials} onChange={(e) => { data.materials = e.target.value }} size='small' fullWidth={true} label="Materials" variant="outlined" required />
+                    <TextField defaultValue={data.materials} onChange={(e) => {
+                        data.materials = e.target.value
+                    }} size='small' fullWidth={true} label="Materials" variant="outlined" required />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField value={data.type} onChange={(e) => { data.type = e.target.value }} size='small' fullWidth={true} label="Type" variant="outlined" required />
+                    <TextField defaultValue={data.type} onChange={(e) => {
+                        data.type = e.target.value
+                    }} size='small' fullWidth={true} label="Type" variant="outlined" required />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField value={data.url} onChange={(e) => { data.url = e.target.value; }} type="URL" size='small' fullWidth={true} label="URL" variant="outlined" required />
+                    <TextField defaultValue={data.url} onChange={(e) => {
+                        data.url = e.target.value;
+                    }} type="URL" size='small' fullWidth={true} label="URL" variant="outlined" required />
                 </Grid>
 
                 <Grid item xs={3}>
